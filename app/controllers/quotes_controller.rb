f79e89799fa3e622e6efe73565e66beb2e7b9aa2
Austin Coburn <austincoburn41@gmail.com>
2019-07-30 19:21:30 +0000

    Built the about page and added photo and bio

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,11 +8,14 @@ end
 	end
 
 	def create
-		Quote.create(quote_params)
+		@quote = Quote.create(quote_params)
 		if @quote.invalid?
 			flash[:error] = '<strong>Could not save</strong> the date you entered is invalid.'
 		end
 		redirect_to root_path
+	end
+
+	def about
 	end
 
 	private
